393ebfc9f88159d041902f94a772835fe8c8273a
lurkGH <lurkoasistwitch@gmail.com>
2024-10-27 12:11:08 -0500

    chore: add function for parsing player gender, add WIP notes for reference

--- init.lua
+++ init.lua
@@ -6,39 +6,41 @@ vReplacer = {
 }
 
 function setupReplacer()
-    defaultLocalizedNames = TweakDB:GetFlat(localizedNames)
-    AMM = GetMod('AppearanceMenuMod')
-    --idList = AMM.Util.possibleIDs --not correct
+    local defaultLocalizedNames = TweakDB:GetFlat(localizedNames)
+    local AMM = GetMod('AppearanceMenuMod')
 
     if AMM ~= nil then
-        -- To Do: access and modify AMM.Util possibleIDs variable
+        --[[ To Do:
+            * access and modify AMM.Util.possibleIDs to exclude "0x9EDC71E0, 33" when this mod is installed
+                - Access to AMM seems to be limited to init.lua; doesn't seem to be a way to access AMM Modules (Util needed)
+            * AMM also populates appearances list for V entity_id = "0x9EDC71E0, 33" using photomode_npc_npv_fem1|2.ent
+                - Not sure if this is intended
+                - Complicates generating different appearances lists for male V or NPC replacer entities
+                - Might not be able to fix outside of AMM
+        ]]
+    end
+
+    for i, entry in ipairs(config.puppetTable) do
+        TweakDB:SetFlat(entry.characterRecord, entry.path)
     end
 
     TweakDB:SetFlat(localizedNames, {newLocalizedName, defaultLocalizedNames[2], defaultLocalizedNames[3]})
 
-    for i, entry in ipairs(config.entityPaths) do
-        TweakDB:SetFlat(entry.replacerEntity, entry.entityPath)
-    end
-
-    -- To Do: ImGui options for entity swapping / AMM patch and pull request
+    --[[ To Do:
+        * ImGui options for entity swapping / AMM patch and pull request
+        * Call updatePlayerGender() on save load
+    ]] 
 
 end
 
-registerForEvent('onInit', function()
-    vReplacer.ready = true
-end)
+function updatePlayerGender()
+    vReplacer.playerGender = string.gmatch(tostring(Game.GetPlayer():GetResolvedGenderName()), '%-%-%[%[%s*(%a+)%s*%-%-%]%]')()
+end
 
 registerForEvent('onTweak', setupReplacer)
 
-registerForEvent('onUpdate', function()
-    -- To Do: implement better than onUpdate, implement check for reload of saves
-    if vReplacer.playerGender == nil then
-        vReplacer.playerGender = Game.GetPlayer():GetResolvedGenderName()
-        -- To Do: parse return value for "Male" or "Female" ^^
-        if vReplacer.playerGender ~= nil then
-            print(vReplacer.playerGender)
-        end
-    end
+registerForEvent('onInit', function()
+    vReplacer.ready = true
 end)
 
 return vReplacer
